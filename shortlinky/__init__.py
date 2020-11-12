76394c11de97b1681e93b135a7103fc645282dd8
Fathin Luqman Tantowi <fltfathin@gmail.com>
2020-11-12 09:31:24 +0700

    frontend beres

--- shortlinky/__init__.py
+++ shortlinky/__init__.py
@@ -6,9 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from secrets import token_urlsafe
 
 url_re = re.compile(
-    r"(http[s]?://)?(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-)
-
+    r"(http[s]?://)?(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
 
 def create_app(test_config=None):
     # create and configure the app
